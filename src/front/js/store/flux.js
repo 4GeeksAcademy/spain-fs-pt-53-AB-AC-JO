5f6aca5b1634452ab2f307fb8595c7441e33ba0b
Ángel Bielsa <145146806+abielsaf@users.noreply.github.com>
2024-03-08 18:47:36 +0000

    Modified back and front URLs, login working

--- src/front/js/store/flux.js
+++ src/front/js/store/flux.js
@@ -30,7 +30,7 @@ const getState = ({ getStore, getActions, setStore }) => {
 			},
 			login: async (email, password) => {
 				try {
-					const res = await fetch("https://expert-winner-5gx76wgr744f7w4w-3001.app.github.dev/api/token", {
+					const res = await fetch("https://crispy-space-umbrella-4j79xjxrj54j2qrpj-3001.app.github.dev/api/token", {
 						method: 'POST',
 						body: JSON.stringify({
 							email: email,
@@ -63,7 +63,7 @@ const getState = ({ getStore, getActions, setStore }) => {
 			},
 			register: async (email, password) => {
 				try {
-					const res = await fetch("https://expert-winner-5gx76wgr744f7w4w-3001.app.github.dev/api/user", {
+					const res = await fetch("https://crispy-space-umbrella-4j79xjxrj54j2qrpj-3001.app.github.dev/api/user", {
 						method: 'POST',
 						body: JSON.stringify({
 							email: email,
@@ -90,7 +90,7 @@ const getState = ({ getStore, getActions, setStore }) => {
 			getMessage: async () => {
 				const store = getStore();
 				try {
-					const resp = await fetch("https://expert-winner-5gx76wgr744f7w4w-3001.app.github.dev/api/hello", {
+					const resp = await fetch("https://crispy-space-umbrella-4j79xjxrj54j2qrpj-3001.app.github.dev/api/hello", {
 						headers: {
 							'Authorization': 'Bearer ' + store.token
 						}
@@ -106,7 +106,7 @@ const getState = ({ getStore, getActions, setStore }) => {
 			getUser: async () => {
 				const store = getStore();
 				try {
-					const resp = await fetch("https://expert-winner-5gx76wgr744f7w4w-3001.app.github.dev/api/privateuser", {
+					const resp = await fetch("https://crispy-space-umbrella-4j79xjxrj54j2qrpj-3001.app.github.dev/api/privateuser", {
 						headers: {
 							'Authorization': 'Bearer ' + store.token
 						}
