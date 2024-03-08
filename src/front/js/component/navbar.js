655822ec34de1baaf35a99cebb976ca920f7da31
AnaMCS26 <144897504+AnaMCS26@users.noreply.github.com>
2024-03-08 18:08:12 +0000

    login ok

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -1,7 +1,14 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useContext } from "react";
+import { Context } from "../store/appContext";
+import { Link, Navigate } from "react-router-dom";
 
 export const Navbar = () => {
+	const { store, actions } = useContext(Context);
+
+	const handleClick = () => {
+		actions.logout()
+	}
+
 	return (
 		<nav className="navbar navbar-light bg-light">
 			<div className="container">
@@ -9,9 +16,12 @@ export const Navbar = () => {
 					<span className="navbar-brand mb-0 h1">React Boilerplate</span>
 				</Link>
 				<div className="ml-auto">
-					<Link to="/demo">
-						<button className="btn btn-primary">Check the Context in action</button>
-					</Link>
+					{!store.token ? <Link to="/login">
+						<button className="btn btn-success">Log in</button>
+					</Link> :
+						<button onClick={handleClick} className="btn btn-danger">Log out</button>
+					}
+
 				</div>
 			</div>
 		</nav>
