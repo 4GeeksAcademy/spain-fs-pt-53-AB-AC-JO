e96833d4b81c0cde8bdb97cff166a9d250f9b71d
Ángel Bielsa <145146806+abielsaf@users.noreply.github.com>
2024-03-08 11:59:56 +0000

    Added button for Añadir review, missing the onclick and the functionality

--- src/front/js/component/results.js
+++ src/front/js/component/results.js
@@ -1,27 +1,29 @@
 import React from 'react';
+import { Card, Button } from 'react-bootstrap';
 
 export const Results = ({ book }) => {
-    return (
-      <div className="card" style={{ width: "18rem" }}>
-        <img src={book.volumeInfo.imageLinks?.thumbnail} className="card-img-top" alt="book cover" />
-        <div className="card-body">
-          <h5 className="card-title">{book.volumeInfo.title}</h5>
-          <p className="card-text">
-            <strong>Author(s):</strong>{" "}
-            {book.volumeInfo.authors?.map((author, index) => (
-              <span key={index}>
-                {author}
-                {index < book.volumeInfo.authors.length - 1 ? ", " : ""}
-              </span>
-            ))}
-          </p>
-          <p className="card-text">
-            <strong>Published Date:</strong> {book.volumeInfo.publishedDate}
-          </p>
-          <p className="card-text">
-            <strong>Page Count:</strong> {book.volumeInfo.pageCount}
-          </p>
-        </div>
-      </div>
-    );
-  }
+  return (
+    <Card style={{ width: '18rem' }} className="mb-4">
+      <Card.Img variant="top" src={book.volumeInfo.imageLinks?.thumbnail} />
+      <Card.Body>
+        <Card.Title>{book.volumeInfo.title}</Card.Title>
+        <Card.Text>
+          <strong>Author(s):</strong>{" "}
+          {book.volumeInfo.authors?.map((author, index) => (
+            <span key={index}>
+              {author}
+              {index < book.volumeInfo.authors.length - 1 ? ", " : ""}
+            </span>
+          ))}
+        </Card.Text>
+        <Card.Text>
+          <strong>Published Date:</strong> {book.volumeInfo.publishedDate}
+        </Card.Text>
+        <Card.Text>
+          <strong>Page Count:</strong> {book.volumeInfo.pageCount}
+        </Card.Text>
+        <Button>Añadir review</Button>
+      </Card.Body>
+    </Card>
+  );
+}
