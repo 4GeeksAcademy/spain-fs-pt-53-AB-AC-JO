7b4b35b34efab352c376766a3a671540c34ca111
Ángel Bielsa <145146806+abielsaf@users.noreply.github.com>
2024-03-12 17:22:13 +0000

    Modified admin.py so it does not show the hash column for security reasons

--- src/api/admin.py
+++ src/api/admin.py
@@ -1,20 +1,22 @@
 import os
 from flask_admin import Admin
-from .models import db, User, Book, Review
 from flask_admin.contrib.sqla import ModelView
+from .models import db, User, Book, Review
+
+class CustomUserView(ModelView):
+    column_list = ('email', 'is_active', 'username', 'visibility')
 
 def setup_admin(app):
     app.secret_key = os.environ.get('FLASK_APP_KEY', 'sample key')
     app.config['FLASK_ADMIN_SWATCH'] = 'cerulean'
     admin = Admin(app, name='4Geeks Admin', template_mode='bootstrap3')
 
-    
     # Add your models here, for example this is how we add a the User model to the admin
-    admin.add_view(ModelView(User, db.session))
+    admin.add_view(CustomUserView(User, db.session))
     admin.add_view(ModelView(Book, db.session))
     admin.add_view(ModelView(Review, db.session))
 
-    # You can duplicate that line to add mew models
+    # You can duplicate that line to add new models
     # admin.add_view(ModelView(YourModelName, db.session))
 
-        # Query all users
+    # Query all users
