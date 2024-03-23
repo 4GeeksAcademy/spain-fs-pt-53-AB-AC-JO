e408c899dbfbc86f8b20eb1f68f23e9e5ef211b8
Ángel Bielsa <145146806+abielsaf@users.noreply.github.com>
2024-03-23 17:18:10 +0000

    ..

--- src/front/js/layout.js
+++ src/front/js/layout.js
@@ -16,7 +16,6 @@ import { ModifyReview } from "./pages/modifyReview";
 import { Navbar } from "./component/navbar";
 import { Footer } from "./component/footer";
 
-
 const Layout = () => {
     //the basename is used when your project is published in a subdirectory and not in the root of the domain
     // you can set the basename on the .env file located at the root of this project, E.g: BASENAME=/react-hello-webapp/
